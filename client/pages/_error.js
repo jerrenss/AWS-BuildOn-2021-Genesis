134f0845a516f00aa46ad1a8a1f868a78958fde7
jerrenss <gerrenseow@gmail.com>
2021-07-20 20:40:24 +0800

    feat: update error page UI

--- client/pages/_error.js
+++ client/pages/_error.js
@@ -1,10 +1,27 @@
-import React from "react";
-import Router from "next/router";
+import Link from 'next/link';
 
-export default function _error() {
-  React.useEffect(() => {
-    Router.push("/admin/dashboard");
-  });
+const MyCustom404Page = () => {
+  return (
+    <div
+      style={{
+        marginTop: 100,
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <h1>404</h1>
+      <h2>
+        <Link href="/">
+          <a style={{ color: 'blue', textDecoration: 'underline', fontSize: '16px' }}>
+            Go To Home Page
+          </a>
+        </Link>
+      </h2>
+      <p>Sorry, the content you are looking for could not be found.</p>
+    </div>
+  );
+};
 
-  return <div />;
-}
+export default MyCustom404Page;
